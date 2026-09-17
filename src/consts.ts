536e8a5764ce5ff51dab510d16c3c92076355d83
shpilson <kovalenko.jora@gmail.com>
2026-09-17 00:06:14 -0300

    fix

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'shpilzen.space';
-export const SITE_DESCRIPTION = 'shpilzen space site description';
+export const SITE_TITLE = 'shpilzen blog';
+export const SITE_DESCRIPTION = 'shpilzen blog site description';
